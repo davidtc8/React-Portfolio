dfa3a1823efbde0157e3b42b26f58b286be93798
David Torres <davidtorresc8@gmail.com>
2022-11-03 17:49:15 -0600

    Images JSON File

---
+++ src/data.js
@@ -0,0 +1,100 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: 'Formula 1 Searcher',
+        img: 'assets/formula1.jpg'
+    },
+    {
+        id: 2,
+        title: 'JS Quiz',
+        img: 'assets/quizjs.jpg'
+    },
+    {
+        id: 3,
+        title: 'Weather Dashboard',
+        img: 'assets/weatherjs.jpg'
+    },
+    {
+        id: 4,
+        title: 'Social Network API',
+        img: 'assets/social_networkjs.jpg'
+    },
+    {
+        id: 5,
+        title: 'RPG Game',
+        img: 'assets/rpg_game.jpg'
+    },
+    {
+        id: 6,
+        title: 'Note Taker',
+        img: 'assets/note_taker.jpg'
+    },
+    {
+        id: 7,
+        title: 'International Space Station Tracker',
+        img: 'assets/international_space.jpg'
+    },
+    {
+        id: 8,
+        title: 'Stock Monitoring Bot',
+        img: 'assets/stocks.jpg'
+    },
+    {
+        id: 9,
+        title: 'Team Profile Geneartor',
+        img: 'assets/team_profile.jpg'
+    },
+    {
+        id: 10,
+        title: 'Workout Tracker Generator',
+        img: 'assets/workout.jpg'
+    },
+    {
+        id: 11,
+        title: 'California Housing Prices',
+        img: 'assets/housing_price.jpg'
+    },
+    {
+        id: 12,
+        title: 'California Housing Prices',
+        img: 'assets/fuel_economy.jpg'
+    },
+    {
+        id: 13,
+        title: 'Decision Trees and Random Forests',
+        img: 'assets/decision_tree.jpg'
+    },
+]
+
+export const softwareDevPortfolio = [
+    {
+        id: 1,
+        title: 'Formula 1 Searcher',
+        img: 'assets/formula1.jpg'
+    },
+    {
+        id: 2,
+        title: 'JS Quiz',
+        img: 'assets/quizjs.jpg'
+    },
+    {
+        id: 3,
+        title: 'Weather Dashboard',
+        img: 'assets/weatherjs.jpg'
+    },
+    {
+        id: 4,
+        title: 'Social Network API',
+        img: 'assets/social_networkjs.jpg'
+    },
+    {
+        id: 5,
+        title: 'RPG Game',
+        img: 'assets/rpg_game.jpg'
+    },
+    {
+        id: 6,
+        title: 'Note Taker',
+        img: 'assets/note_taker.jpg'
+    },
+]
