d6b5702a044283bfe4e4308c1bac851ff324de58
David Torres <davidtorresc8@gmail.com>
2022-11-05 16:28:25 -0600

    Contact Component

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,27 @@
 import './contact.scss'
+import {useState} from 'react'
 
-export default function contact() {
+export default function Contact() {
+  const [message, setMessage] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true)
+  }
   return (
     <div className='contact' id='contact'>
-        contact
+        <div className='left'>
+          <img src="assets/dino.png" alt="" />
+        </div>
+        <div className='right'>
+          <h1>Contact</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Email'/>
+            <textarea placeholder='Message'></textarea>
+            <button type='submit'>Send</button>
+            {message && <span>Thanks for reaching out! I'll reply ASAP.</span>}
+          </form>
+        </div>
     </div>
   )
 }
