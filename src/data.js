3021e43b8c0d0dbece230cc4948e848a2eba5f89
David Torres <davidtorresc8@gmail.com>
2022-11-05 16:28:03 -0600

    Data for portfolio

--- src/data.js
+++ src/data.js
@@ -2,67 +2,50 @@ export const featuredPortfolio = [
     {
         id: 1,
         title: 'Formula 1 Searcher',
-        img: 'assets/formula1.jpg'
+        img: 'assets/formula1.jpg',
+        description: 'Using a Formula 1 API, we created webpage where you can search career by year and other topics!',
+        tech: '#API #Javascript #FrontEnd',
+        link: 'https://davidtc8.github.io/Formula1-Webpage/'
     },
     {
         id: 2,
-        title: 'JS Quiz',
-        img: 'assets/quizjs.jpg'
+        title: 'Social Network API',
+        img: 'assets/social_networkjs.jpg',
+        description: 'API for a social network web application where users can share their thoughts, react to friends thoughts, and create a friend list.',
+        tech: '#MongoDB #Javascript #Backend',
+        link: 'https://github.com/davidtc8/Social-Network-API'
     },
     {
         id: 3,
-        title: 'Weather Dashboard',
-        img: 'assets/weatherjs.jpg'
+        title: 'RPG Game',
+        img: 'assets/rpg_game.jpg',
+        description: 'My first RPG Game in Python! 🕹️ Using Object Oriented Programming.',
+        tech: '#Python #OOP #Backend',
+        link: 'https://github.com/davidtc8/My_First_RPG_Game'
     },
     {
         id: 4,
-        title: 'Social Network API',
-        img: 'assets/social_networkjs.jpg'
+        title: 'International Space Station Tracker',
+        img: 'assets/international_space.jpg',
+        description: 'Python bot 🤖 that tracks where is the International Space Station (ISS) 🚀 currently at the sky.',
+        tech: '#Python #Backend #API',
+        link: 'https://github.com/davidtc8/International_Space_Station_Tracker'
     },
     {
         id: 5,
-        title: 'RPG Game',
-        img: 'assets/rpg_game.jpg'
+        title: 'Team Profile Geneartor',
+        img: 'assets/team_profile.jpg',
+        description: 'App that takes information about employees on a Software Engineering Team and generates an HTML webpage.',
+        tech: '#NodeJS #FrontEnd #Backend',
+        link: 'https://github.com/davidtc8/team-profile-gen'
     },
     {
         id: 6,
-        title: 'Note Taker',
-        img: 'assets/note_taker.jpg'
-    },
-    {
-        id: 7,
-        title: 'International Space Station Tracker',
-        img: 'assets/international_space.jpg'
-    },
-    {
-        id: 8,
-        title: 'Stock Monitoring Bot',
-        img: 'assets/stocks.jpg'
-    },
-    {
-        id: 9,
-        title: 'Team Profile Geneartor',
-        img: 'assets/team_profile.jpg'
-    },
-    {
-        id: 10,
-        title: 'Workout Tracker Generator',
-        img: 'assets/workout.jpg'
-    },
-    {
-        id: 11,
-        title: 'California Housing Prices',
-        img: 'assets/housing_price.jpg'
-    },
-    {
-        id: 12,
-        title: 'California Housing Prices',
-        img: 'assets/fuel_economy.jpg'
-    },
-    {
-        id: 13,
-        title: 'Decision Trees and Random Forests',
-        img: 'assets/decision_tree.jpg'
+        title: 'Fuel Economy',
+        img: 'assets/fuel_economy.jpg',
+        description: 'Data Analysis with Pandas, Numpy and Matplotlib 📈 about a fuel economy data 💸 from US car models 🚗 from 2008 and 2018',
+        tech: '#Python #Pandas #Matplotlib #Data Analysis',
+        link: 'https://github.com/davidtc8/Fuel_Economy_Data_Analysis'
     },
 ]
 
@@ -70,31 +53,111 @@ export const softwareDevPortfolio = [
     {
         id: 1,
         title: 'Formula 1 Searcher',
-        img: 'assets/formula1.jpg'
+        img: 'assets/formula1.jpg',
+        description: 'Using a Formula 1 API, we created webpage where you can search career by year and other topics!',
+        tech: '#API #Javascript #FrontEnd',
+        link: 'https://davidtc8.github.io/Formula1-Webpage/'
     },
     {
         id: 2,
         title: 'JS Quiz',
-        img: 'assets/quizjs.jpg'
+        img: 'assets/quizjs.jpg',
+        description: "Dynamic quiz where you'll be able to save your scores.",
+        tech: '#Javascript #LocalStorage #Advanced CSS',
+        link: 'https://davidtc8.github.io/JS-Quiz/'
     },
     {
         id: 3,
         title: 'Weather Dashboard',
-        img: 'assets/weatherjs.jpg'
+        img: 'assets/weatherjs.jpg',
+        description: 'Dashboard where you will retrieve weather data for cities using a Weather API.        ',
+        tech: '#API #Javascript #LocalStorage',
+        link: 'https://davidtc8.github.io/Weather-Dashboard-JS/'
     },
     {
         id: 4,
         title: 'Social Network API',
-        img: 'assets/social_networkjs.jpg'
+        img: 'assets/social_networkjs.jpg',
+        description: 'API for a social network web application where users can share their thoughts, react to friends thoughts, and create a friend list.',
+        tech: '#MongoDB #Javascript #Backend',
+        link: 'https://github.com/davidtc8/Social-Network-API' 
     },
     {
         id: 5,
         title: 'RPG Game',
-        img: 'assets/rpg_game.jpg'
+        img: 'assets/rpg_game.jpg',
+        description: 'My first RPG Game in Python! 🕹️ Using Object Oriented Programming.',
+        tech: '#Python #OOP #Backend',
+        link: 'https://github.com/davidtc8/My_First_RPG_Game'
     },
     {
         id: 6,
         title: 'Note Taker',
-        img: 'assets/note_taker.jpg'
+        img: 'assets/note_taker.jpg',
+        description: "Website where you'll be able to save your daily tasks and save them for upcomming days!",
+        tech: '#Express #Heroku #Database #NodeJS',
+        link: 'https://note-taking-app-js.herokuapp.com/'
+    },
+]
+
+export const automationPortfolio = [
+    {
+        id: 1,
+        title: 'International Space Station Tracker',
+        img: 'assets/international_space.jpg',
+        description: 'Python bot 🤖 that tracks where is the International Space Station (ISS) 🚀 currently at the sky.',
+        tech: '#Python #Backend #API',
+        link: 'https://github.com/davidtc8/International_Space_Station_Tracker'
+    },
+    {
+        id: 2,
+        title: 'Stock Monitoring Bot',
+        img: 'assets/stocks.jpg',
+        description: 'Python code that analyses how stocks behave and gain insights about why they went up or down!',
+        tech: '#API #List Comprehension #Email Sending',
+        link: 'https://github.com/davidtc8/Stock-Monitoring-Project'
+    },
+    {
+        id: 3,
+        title: 'Team Profile Geneartor',
+        img: 'assets/team_profile.jpg',
+        description: 'App that takes information about employees on a Software Engineering Team and generates an HTML webpage.',
+        tech: '#NodeJS #FrontEnd #Backend',
+        link: 'https://github.com/davidtc8/team-profile-gen'
+    },
+    {
+        id: 4,
+        title: 'Workout Tracker Generator',
+        img: 'assets/workout.jpg',
+        description: 'Workout tracker that you can use to keep track of each excersice you do!',
+        tech: '#HTTP Requests #API #Python',
+        link: 'https://github.com/davidtc8/Workout_Tracker'
+    },
+]
+
+export const aiDataAnalysisPortfolio = [
+    {
+        id: 1,
+        title: 'California Housing Prices',
+        img: 'assets/housing_price.jpg',
+        description: 'Analyzing the housing prices of California EEUU using Python.',
+        tech: '#Python #Pandas #Machine Learning',
+        link: 'https://github.com/davidtc8/California-Housing-Prices'
+    },
+    {
+        id: 2,
+        title: 'Fuel Economy',
+        img: 'assets/fuel_economy.jpg',
+        description: 'Data Analysis with Pandas, Numpy and Matplotlib 📈 about a fuel economy data 💸 from US car models 🚗 from 2008 and 2018',
+        tech: '#Python #Pandas #Matplotlib #Data Analysis',
+        link: 'https://github.com/davidtc8/Fuel_Economy_Data_Analysis'
+    },
+    {
+        id: 3,
+        title: 'Decision Trees and Random Forests',
+        img: 'assets/decision_tree.jpg',
+        description: 'Deciding if a certain individual can be a target to aquire a credit from a bank using random forests',
+        tech: '#Python #Machine Learning #AI',
+        link: 'https://github.com/davidtc8/MSC-Decision_tree_and_random_forests'
     },
 ]
